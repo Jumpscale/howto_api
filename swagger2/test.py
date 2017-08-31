110390454806e2e7557b73716783c7f1831f83a5
root <kristof@incubaid.com>
2017-08-31 08:08:06 +0000

    raml fixes

--- swagger2/test.py
+++ swagger2/test.py
@@ -1,1 +1,16 @@
+from js9 import j
+
+from bravado.requests_client import RequestsClient
 from bravado.client import SwaggerClient
+
+http_client = RequestsClient()
+
+# http_client.set_api_key("my.zerotier.com", api_key="", param_name='api_key', param_in='query')
+http_client.session.headers["Authorization"] = "bearer MQMmvJEGWfAccGYQ93UMjxn5vMSu4q2m"
+
+# dd = j.data.serializer.json.load("zerotier_swagger.json")
+dd = j.data.serializer.json.load("zerotier_1file.json")
+cl = SwaggerClient.from_spec(dd, http_client=http_client)
+
+from IPython import embed
+embed(colors='Linux')
